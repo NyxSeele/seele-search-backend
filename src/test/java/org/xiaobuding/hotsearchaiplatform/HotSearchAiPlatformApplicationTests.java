ceffddef836ed97539b182a9e46ce8616e09e7dc
Xiaobuding <13567779341@qq.com>
2025-11-18 23:03:18 +0800

    2025.11.18 无BUG

---
+++ src/test/java/org/xiaobuding/hotsearchaiplatform/HotSearchAiPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xiaobuding.hotsearchaiplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotSearchAiPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
